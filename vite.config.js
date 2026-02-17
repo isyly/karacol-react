70869898e0938fa5875ce99b9b237799a839a8d4
isyly <79649023+isyly@users.noreply.github.com>
2026-02-17 16:37:23 +0100

    test commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/karacol-react/',
+  base: '/karacol-react',
   plugins: [react()],
 })
